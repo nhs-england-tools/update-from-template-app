203a86319fc6a91e51c95e0a97a56e031d12f4ef
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2023-07-06 07:56:09 +0100

    Fix issue with file names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
-/compare-directories --dir1=$1 --dir2=$2 > ./output.txt
+/compare-directories --dir1=$1 --dir2=$2 --exclude=.git/ > ./output.txt
 cat ./output.txt
 
 set -x
-files=$(cat ./output.txt | jq '.comparison | keys[]')
+files=$(cat ./output.txt | jq -r '.comparison | keys[]')
 for file in $files; do
   dir=$(dirname "$file")
   mkdir -p $2/$dir
