0113c14530c88b8c96a1f7fc225d83d0e3b93c52
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2023-08-06 16:44:45 +0100

    Handle new output format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,28 @@
 #!/bin/sh -l
 
-/compare-directories --dir1=$1 --dir2=$2 --cfg=$3 > ./output.txt
-cat ./output.txt
+/compare-directories \
+  --source-dir $1 \
+  --destination-dir $2 \
+  --config-file $3
+> ./output.json
+cat ./output.json
 
 set -x
-files=$(cat ./output.txt | jq -r '.comparison | keys[]')
-for file in $files; do
+
+to_copy=$(
+  cat ./output.json \
+    | jq -r '.comparison | to_entries[] | select(.value.action == "copy") | .key'
+)
+for file in $to_copy; do
   dir=$(dirname "$file")
   mkdir -p $2/$dir
   cp $1/$file $2/$file
+done
+
+to_delete=$(
+  cat ./output.json \
+    | jq -r '.comparison | to_entries[] | select(.value.action == "delete") | .key'
+)
+for file in $to_delete; do
+  rm -rf $2/$file
 done
