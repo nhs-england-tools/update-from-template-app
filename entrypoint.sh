03bfbae52b0227bd8d20fa1d32294401854d22ea
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2023-08-08 08:26:17 +0100

    Improve configuration of the shell script to run on GitHub Action

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,23 @@ dest_dir=${GITHUB_WORKSPACE:-/github/workspace}/${DESTINATION_DIR}
 
 # ==============================================================================
 
+# Global git settings
+git config --global user.name "${GIT_USER_NAME}"
+git config --global user.email "${GIT_USER_EMAIL}@users.noreply.github.com"
+git config --global pull.rebase false
+git config --global --add safe.directory ${GITHUB_WORKSPACE:-/github/workspace}
+
 cd ${dest_dir}
-git config user.name "${GIT_USER_NAME}"
-git config user.email "${GIT_USER_EMAIL}@users.noreply.github.com"
-git config --global --add safe.directory ${dest_dir}
-# Close PRs
+# Close legacy PRs
 pr_numbers=$(gh pr list --search "Update from template" --json number,title | jq '.[] | select(.title | startswith("Update from template")).number')
 for pr_number in $pr_numbers; do
   gh pr close $pr_number
 done
-# Delete branches
+# Delete legacy branches
 for branch in $(git branch -r | grep 'origin/update-from-template'); do
   git push origin --delete ${branch#origin/}
 done
+# Create new branch
 git checkout -b update-from-template-${BUILD_DATETIME}
 
 # ==============================================================================
@@ -49,9 +53,11 @@ done
 
 # ==============================================================================
 
+# Commit and push changes
 git add -A
 git commit -m "Update from template $(date --date=${BUILD_DATETIME} +'%Y-%m-%dT%H:%M:%S%z')"
 git push -u origin update-from-template-$(date --date=${BUILD_DATETIME} -u +'%Y%m%d%H%M%S')
+# Create new PR
 gh pr create \
   --title "Update from template" \
   --body "Update from template $(date --date=${BUILD_DATETIME} +'%Y-%m-%dT%H:%M:%S%z')"
